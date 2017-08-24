b28a75c0dc2792c73bac522cd09486bf8c9b56f9
rragundez <rragundez@users.noreply.github.com>
2017-08-24 16:36:08 +0200

    Update utils.py

--- app/resources/utils.py
+++ app/resources/utils.py
@@ -2,19 +2,18 @@ import base64
 import io
 
 
-def pandas_plot_to_html(fig, img_format='png'):
+def pandas_plot_to_html(fig):
     """Convert a matplotlib figure object to an html element
 
     Args:
         fig (matplotlib.figure.Figure): Figure to be plotted.
-        img_format (str): Format of the image ot produce.
 
     Returns:
         str: An html img element containig the figure sent.
     """
     img = io.BytesIO()
     fig.set_tight_layout(True)
-    fig.savefig(img, format=img_format)
+    fig.savefig(img, format='png')
     img_str = base64.b64encode(img.getvalue()).decode()
     html = '<img src="data:image/png;base64,{}"/>'.format(img_str)
     return html
